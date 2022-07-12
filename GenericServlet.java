bc1960c600637f830b272bee99ec9ffa308b1e4a
satyajeet kumar <55208293+Satyajeetkumar2019@users.noreply.github.com>
2022-07-13 00:23:25 +0530

    Add some programe here

---
+++ GenericServlet.java
@@ -0,0 +1,13 @@
+package com.nt.DateServlet;
+import java.util.*;
+import java.io.*;
+
+public class GenericServlet {
+
+	public static void main(String[]args) {
+		System.out.println("Hello satyajeet kumar");
+		
+		
+		
+	} 
+}
